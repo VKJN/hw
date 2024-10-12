3d9c2681387f7982ad08cab3037206e141c350cc
Alexandr Nosachev <nosachev2512@gmail.com>
2024-10-12 20:22:57 +0300

    Hw is done

---
+++ hello.pas
@@ -0,0 +1,12 @@
+﻿begin
+  Print($'1) Русский {newline}2) English {newline}3) Deutsch{newline}');
+  var val := ReadInteger('Введите номер языка, на котором вы хотите увидеть приветствие: ');
+  
+  case val of
+    1: Print('Привет, мир!');
+    2: Print('Hello world');
+    3: Print('Hallo Welt!');
+    else
+      Print('Error');
+  end;
+end.
